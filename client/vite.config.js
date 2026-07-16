5e0a36e57d76bac0532f1cbbd2282e735f671f24
lazobasic <lazo.basic@gmail.com>
2026-07-16 15:25:05 +0100

    Add Vite config

---
+++ client/vite.config.js
@@ -0,0 +1,1 @@
+import { defineConfig } from 'vite';\nimport react from '@vitejs/plugin-react';\n\nexport default defineConfig({\n  plugins: [react()],\n  server: {\n    proxy: {\n      '/api': 'http://localhost:5000'\n    }\n  }\n});\n
